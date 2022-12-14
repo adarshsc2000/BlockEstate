dd975133490e8e41acb7b70147bfe14e3d5ec730
AdarshChandran2000 <lachikku2210@gmail.com>
2022-12-14 16:32:24 +0300

    Bring the graph queries to the client

--- client/pages/browse/index.jsx
+++ client/pages/browse/index.jsx
@@ -1,28 +1,38 @@
 import React, { useEffect, useState } from "react";
 import Navigationbar from "../../components/Navigationbar.jsx";
-import Meta from "../../components/Meta.jsx"
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import Meta from "../../components/Meta.jsx";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 import { Container } from "react-bootstrap";
 import { Row, Col } from "react-bootstrap";
-import Stack from 'react-bootstrap/Stack';
+import Stack from "react-bootstrap/Stack";
 import { FaMapMarkerAlt } from "react-icons/fa";
-import { BiBed, BiBath } from "react-icons/bi"
-import { FiMessageCircle } from "react-icons/fi"
-import { RxDimensions } from "react-icons/rx"
+import { BiBed, BiBath } from "react-icons/bi";
+import { FiMessageCircle } from "react-icons/fi";
+import { RxDimensions } from "react-icons/rx";
 import BrowsePropertyCard from "../../components/BrowsePropertyCard.jsx";
-import properties from '../../properties.js'
+import properties from "../../properties.js";
+
+// graph
+import { useQuery } from "@apollo/client";
+import GET_ACTIVE_PROPERTIES from "../../constants/subgraphQueries";
 
 //import {isWeb3Enabled} from useMoralis;
 
-
 export default function browse(props) {
+  // graph test
+  const { loading, error, data: listedProperties } = useQuery(GET_ACTIVE_PROPERTIES);
+  loading || !listedProperties
+    ? console.log("Loading...")
+    : listedProperties.activeProperties.map((property) => console.log(property));
+  // end graph test
+
   /*   const [provider, setProvider] = useState({})
 
     useEffect(() => {
       setProvider(new ethers.providers.Web3Provider(window.ethereum))
     }, []); */
-  const [isConnectedToWallet, setIsConnectedToWallet] = useState(true) //made true for testing , should be isWeb3Enabled
+  const [isConnectedToWallet, setIsConnectedToWallet] = useState(true); //made true for testing , should be isWeb3Enabled
   /*   useEffect(function () {
       if (isWeb3Enabled )
         setIsConnectedToWallet(true)
@@ -32,10 +42,23 @@ export default function browse(props) {
  */
 
   const propertyCardElements = properties.map((property, index) => {
-
-    return <BrowsePropertyCard key={index} property_id={property.property_id} image={property.images[0]} propertyType={property.propertyType} priceInBhd={property.priceInBhd} description={property.description} location={property.location} bedrooms={property.bedrooms} bathrooms={property.bathrooms} propertyArea={property.propertyArea} postDate={property.postDate} phoneNumber={property.phoneNumber} />
-
-  })
+    return (
+      <BrowsePropertyCard
+        key={index}
+        property_id={property.property_id}
+        image={property.images[0]}
+        propertyType={property.propertyType}
+        priceInBhd={property.priceInBhd}
+        description={property.description}
+        location={property.location}
+        bedrooms={property.bedrooms}
+        bathrooms={property.bathrooms}
+        propertyArea={property.propertyArea}
+        postDate={property.postDate}
+        phoneNumber={property.phoneNumber}
+      />
+    );
+  });
 
   const [domLoaded, setDomLoaded] = useState(false);
   useEffect(() => {
@@ -49,17 +72,25 @@ export default function browse(props) {
       <br /> <br />
       <Container>
         <Stack gap={4}>
-
-          <BrowsePropertyCard image="pic1prop1.jpeg" propertyType="Apartment" priceInBhd="400" description="Sea view apartment with many....slice method used to truim the string to 11 chars, so 2 lines on md screen. otherwise img does not fit card" location="Abraj Al Lulu, Manama, Capital Governate" bedrooms="8" bathrooms="8" propertyArea="736" postDate="5/12/2022" phoneNumber="97333344444" />
+          <BrowsePropertyCard
+            image="pic1prop1.jpeg"
+            propertyType="Apartment"
+            priceInBhd="400"
+            description="Sea view apartment with many....slice method used to truim the string to 11 chars, so 2 lines on md screen. otherwise img does not fit card"
+            location="Abraj Al Lulu, Manama, Capital Governate"
+            bedrooms="8"
+            bathrooms="8"
+            propertyArea="736"
+            postDate="5/12/2022"
+            phoneNumber="97333344444"
+          />
 
           {propertyCardElements}
-
         </Stack>
       </Container>
     </div>
-  )
+  );
 }
-
 
 /* export async function getServerSideProps() {
   const res = await fetch()
--- client/public/images.js
+++ client/public/images.js
@@ -1,7 +1,7 @@
-import image1 from 'c:/Users/haifa/Downloads/UOB/SENIOR PROJECT/project2/client/public/assets/pic1prop1.jpeg';
-import image2 from 'c:/Users/haifa/Downloads/UOB/SENIOR PROJECT/project2/client/public/assets/pic2prop1.jpeg';
-import image3 from 'c:/Users/haifa/Downloads/UOB/SENIOR PROJECT/project2/client/public/assets/pic3prop1.jpeg';
-import image4 from 'c:/Users/haifa/Downloads/UOB/SENIOR PROJECT/project2/client/public/assets/pic4prop2.jpeg';
-import image5 from 'c:/Users/haifa/Downloads/UOB/SENIOR PROJECT/project2/client/public/assets/pic6prop2.jpeg';
+import image1 from "/public/assets/pic1prop1.jpeg";
+import image2 from "/public/assets/pic2prop1.jpeg";
+import image3 from "/public/assets/pic3prop1.jpeg";
+import image4 from "/public/assets/pic4prop2.jpeg";
+import image5 from "/public/assets/pic6prop2.jpeg";
 
 export default [image1, image2, image3, image4, image5];
