c6a2fe527a989bed5d550d71796c8599bf44e32c
AdarshChandran2000 <lachikku2210@gmail.com>
2022-12-15 15:11:07 +0300

    Bug fixes in test

--- test/unit/RealEstate.test.ts
+++ test/unit/RealEstate.test.ts
@@ -4,12 +4,12 @@ const { developmentChains } = require("../../helper-hardhat-config");
 
 // types
 import { Signer } from "ethers";
-import { RealEstate, BasicNft } from "../../typechain-types";
+import { BlockEstate, BasicNft } from "../../typechain-types";
 
 !developmentChains.includes(network.name)
     ? describe.skip
-    : describe("Nft Marketplace Unit Tests", function () {
-          let realEstate: RealEstate, realEstateContract: RealEstate, basicNft: BasicNft;
+    : describe("BlockEstate Unit Tests", function () {
+          let blockEstate: BlockEstate, blockEstateContract: BlockEstate, basicNft: BasicNft;
           const PRICE = ethers.utils.parseEther("0.1");
           const TOKEN_ID = 0;
 
@@ -21,44 +21,44 @@ import { RealEstate, BasicNft } from "../../typechain-types";
               deployer = accounts[0];
               user = accounts[1];
               await deployments.fixture(["all"]);
-              realEstateContract = await ethers.getContract("RealEstate");
-              realEstate = realEstateContract.connect(deployer);
+              blockEstateContract = await ethers.getContract("BlockEstate");
+              blockEstate = blockEstateContract.connect(deployer);
               basicNft = await ethers.getContract("BasicNft");
               await basicNft.mintNft();
-              await basicNft.approve(realEstateContract.address, TOKEN_ID);
+              await basicNft.approve(blockEstateContract.address, TOKEN_ID);
           });
 
           describe("listItem", function () {
               it("emits an event after listing an item", async function () {
-                  expect(await realEstate.listItem(basicNft.address, TOKEN_ID, PRICE)).to.emit(
+                  expect(await blockEstate.listItem(basicNft.address, TOKEN_ID, PRICE)).to.emit(
                       "ItemListed"
                   );
               });
               it("exclusively items that haven't been listed", async function () {
-                  await realEstate.listItem(basicNft.address, TOKEN_ID, PRICE);
+                  await blockEstate.listItem(basicNft.address, TOKEN_ID, PRICE);
                   //   await expect(
-                  //       realEstate.listItem(basicNft.address, TOKEN_ID, PRICE)
-                  //   ).to.be.revertedWithCustomError(realEstate, "AlreadyListed")
-                  await expect(realEstate.listItem(basicNft.address, TOKEN_ID, PRICE))
-                      .to.be.revertedWithCustomError(realEstate, "AlreadyListed")
+                  //       blockEstate.listItem(basicNft.address, TOKEN_ID, PRICE)
+                  //   ).to.be.revertedWithCustomError(blockEstate, "AlreadyListed")
+                  await expect(blockEstate.listItem(basicNft.address, TOKEN_ID, PRICE))
+                      .to.be.revertedWithCustomError(blockEstate, "AlreadyListed")
                       .withArgs(basicNft.address, TOKEN_ID);
               });
               it("exclusively allows owners to list", async function () {
-                  realEstate = realEstateContract.connect(user);
+                  blockEstate = blockEstateContract.connect(user);
                   await basicNft.approve(user.getAddress(), TOKEN_ID);
                   await expect(
-                      realEstate.listItem(basicNft.address, TOKEN_ID, PRICE)
-                  ).to.be.revertedWithCustomError(realEstate, "NotOwner");
+                      blockEstate.listItem(basicNft.address, TOKEN_ID, PRICE)
+                  ).to.be.revertedWithCustomError(blockEstate, "NotOwner");
               });
               it("needs approvals to list item", async function () {
                   await basicNft.approve(ethers.constants.AddressZero, TOKEN_ID);
                   await expect(
-                      realEstate.listItem(basicNft.address, TOKEN_ID, PRICE)
-                  ).to.be.revertedWithCustomError(realEstate, "NotApprovedForMarketplace");
+                      blockEstate.listItem(basicNft.address, TOKEN_ID, PRICE)
+                  ).to.be.revertedWithCustomError(blockEstate, "NotApprovedForMarketplace");
               });
               it("Updates listing with seller and price", async function () {
-                  await realEstate.listItem(basicNft.address, TOKEN_ID, PRICE);
-                  const listing = await realEstate.getListing(basicNft.address, TOKEN_ID);
+                  await blockEstate.listItem(basicNft.address, TOKEN_ID, PRICE);
+                  const listing = await blockEstate.getListing(basicNft.address, TOKEN_ID);
                   assert(listing.price.toString() == PRICE.toString());
                   assert(listing.seller.toString() == (await deployer.getAddress()));
               });
@@ -66,46 +66,46 @@ import { RealEstate, BasicNft } from "../../typechain-types";
           describe("cancelListing", function () {
               it("reverts if there is no listing", async function () {
                   await expect(
-                      realEstate.cancelListing(basicNft.address, TOKEN_ID)
-                  ).to.be.revertedWithCustomError(realEstate, 'NotListed').withArgs(basicNft.address, TOKEN_ID);
+                      blockEstate.cancelListing(basicNft.address, TOKEN_ID)
+                  ).to.be.revertedWithCustomError(blockEstate, 'NotListed').withArgs(basicNft.address, TOKEN_ID);
               });
               it("reverts if anyone but the owner tries to call", async function () {
-                  await realEstate.listItem(basicNft.address, TOKEN_ID, PRICE);
-                  realEstate = realEstateContract.connect(user);
+                  await blockEstate.listItem(basicNft.address, TOKEN_ID, PRICE);
+                  blockEstate = blockEstateContract.connect(user);
                   await basicNft.approve(user.getAddress(), TOKEN_ID);
                   await expect(
-                      realEstate.cancelListing(basicNft.address, TOKEN_ID)
-                  ).to.be.revertedWithCustomError(realEstate, "NotOwner");
+                      blockEstate.cancelListing(basicNft.address, TOKEN_ID)
+                  ).to.be.revertedWithCustomError(blockEstate, "NotOwner");
               });
               it("emits event and removes listing", async function () {
-                  await realEstate.listItem(basicNft.address, TOKEN_ID, PRICE);
-                  expect(await realEstate.cancelListing(basicNft.address, TOKEN_ID)).to.emit(
+                  await blockEstate.listItem(basicNft.address, TOKEN_ID, PRICE);
+                  expect(await blockEstate.cancelListing(basicNft.address, TOKEN_ID)).to.emit(
                       "ItemCanceled"
                   );
-                  const listing = await realEstate.getListing(basicNft.address, TOKEN_ID);
+                  const listing = await blockEstate.getListing(basicNft.address, TOKEN_ID);
                   assert(listing.price.toString() == "0");
               });
           });
           describe("buyItem", function () {
               it("reverts if the item isnt listed", async function () {
                   await expect(
-                      realEstate.buyItem(basicNft.address, TOKEN_ID)
-                  ).to.be.revertedWithCustomError(realEstate, "NotListed");
+                      blockEstate.buyItem(basicNft.address, TOKEN_ID)
+                  ).to.be.revertedWithCustomError(blockEstate, "NotListed");
               });
               it("reverts if the price isnt met", async function () {
-                  await realEstate.listItem(basicNft.address, TOKEN_ID, PRICE);
+                  await blockEstate.listItem(basicNft.address, TOKEN_ID, PRICE);
                   await expect(
-                      realEstate.buyItem(basicNft.address, TOKEN_ID)
-                  ).to.be.revertedWithCustomError(realEstate, "PriceNotMet");
+                      blockEstate.buyItem(basicNft.address, TOKEN_ID)
+                  ).to.be.revertedWithCustomError(blockEstate, "PriceNotMet");
               });
               it("transfers the nft to the buyer and updates internal proceeds record", async function () {
-                  await realEstate.listItem(basicNft.address, TOKEN_ID, PRICE);
-                  realEstate = realEstateContract.connect(user);
+                  await blockEstate.listItem(basicNft.address, TOKEN_ID, PRICE);
+                  blockEstate = blockEstateContract.connect(user);
                   expect(
-                      await realEstate.buyItem(basicNft.address, TOKEN_ID, { value: PRICE })
+                      await blockEstate.buyItem(basicNft.address, TOKEN_ID, { value: PRICE })
                   ).to.emit("ItemBought");
                   const newOwner = await basicNft.ownerOf(TOKEN_ID);
-                  const deployerProceeds = await realEstate.getProceeds(deployer.getAddress());
+                  const deployerProceeds = await blockEstate.getProceeds(deployer.getAddress());
                   assert(newOwner.toString() == (await user.getAddress()));
                   assert(deployerProceeds.toString() == PRICE.toString());
               });
@@ -113,42 +113,42 @@ import { RealEstate, BasicNft } from "../../typechain-types";
           describe("updateListing", function () {
               it("must be owner and listed", async function () {
                   await expect(
-                      realEstate.updateListing(basicNft.address, TOKEN_ID, PRICE)
-                  ).to.be.revertedWithCustomError(realEstate, "NotListed");
-                  await realEstate.listItem(basicNft.address, TOKEN_ID, PRICE);
-                  realEstate = realEstateContract.connect(user);
+                      blockEstate.updateListing(basicNft.address, TOKEN_ID, PRICE)
+                  ).to.be.revertedWithCustomError(blockEstate, "NotListed");
+                  await blockEstate.listItem(basicNft.address, TOKEN_ID, PRICE);
+                  blockEstate = blockEstateContract.connect(user);
                   await expect(
-                      realEstate.updateListing(basicNft.address, TOKEN_ID, PRICE)
-                  ).to.be.revertedWithCustomError(realEstate, "NotOwner");
+                      blockEstate.updateListing(basicNft.address, TOKEN_ID, PRICE)
+                  ).to.be.revertedWithCustomError(blockEstate, "NotOwner");
               });
               it("updates the price of the item", async function () {
                   const updatedPrice = ethers.utils.parseEther("0.2");
-                  await realEstate.listItem(basicNft.address, TOKEN_ID, PRICE);
+                  await blockEstate.listItem(basicNft.address, TOKEN_ID, PRICE);
                   expect(
-                      await realEstate.updateListing(basicNft.address, TOKEN_ID, updatedPrice)
+                      await blockEstate.updateListing(basicNft.address, TOKEN_ID, updatedPrice)
                   ).to.emit("ItemListed");
-                  const listing = await realEstate.getListing(basicNft.address, TOKEN_ID);
+                  const listing = await blockEstate.getListing(basicNft.address, TOKEN_ID);
                   assert(listing.price.toString() == updatedPrice.toString());
               });
           });
           describe("withdrawProceeds", function () {
               it("doesn't allow 0 proceed withdrawls", async function () {
-                  await expect(realEstate.withdrawProceeds()).to.be.revertedWithCustomError(
-                      realEstate,
+                  await expect(blockEstate.withdrawProceeds()).to.be.revertedWithCustomError(
+                      blockEstate,
                       "NoProceeds"
                   );
               });
               it("withdraws proceeds", async function () {
-                  await realEstate.listItem(basicNft.address, TOKEN_ID, PRICE);
-                  realEstate = realEstateContract.connect(user);
-                  await realEstate.buyItem(basicNft.address, TOKEN_ID, { value: PRICE });
-                  realEstate = realEstateContract.connect(deployer);
+                  await blockEstate.listItem(basicNft.address, TOKEN_ID, PRICE);
+                  blockEstate = blockEstateContract.connect(user);
+                  await blockEstate.buyItem(basicNft.address, TOKEN_ID, { value: PRICE });
+                  blockEstate = blockEstateContract.connect(deployer);
 
-                  const deployerProceedsBefore = await realEstate.getProceeds(
+                  const deployerProceedsBefore = await blockEstate.getProceeds(
                       deployer.getAddress()
                   );
                   const deployerBalanceBefore = await deployer.getBalance();
-                  const txResponse = await realEstate.withdrawProceeds();
+                  const txResponse = await blockEstate.withdrawProceeds();
                   const transactionReceipt = await txResponse.wait(1);
                   const { gasUsed, effectiveGasPrice } = transactionReceipt;
                   const gasCost = gasUsed.mul(effectiveGasPrice);
